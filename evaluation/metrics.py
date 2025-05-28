7261c558f5e63dd524a65b42fd83400b5a9afe22
Abygaïl Stegenga <a.l.h.stegenga@gmail.com>
2025-05-28 12:37:51 +0200

    Adding metric call function

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -3,8 +3,6 @@ import pandas as pd
 import skimage.measure
 import os
 
-from dill import objects
-
 
 items = ["A", "K", "t", "B", "H"]
 enemies = ["E"]
@@ -115,12 +113,13 @@ def item_distribution(img):
     relation_EF= count_enemies_to_floor(img)
     relation_HE = count_health_to_enemy(img)
     # f"M^2 %s \n items %s", m_sq, objectives,
-    print(
-            f"Enemies to m²: {relation_EF}%\n"
-            f"Enemies in a Map: {relation_E}\n"
-            f"Healths in relation to enemies: {relation_HE}\n"
-            # f"keys: {relation_p}, {doors}"%\n"
-            f"Items per m²: {relation}\n")
+    # print(
+    #         f"Enemies to m²: {relation_EF}%\n"
+    #         f"Enemies in a Map: {relation_E}\n"
+    #         f"Healths in relation to enemies: {relation_HE}\n"
+    #         # f"keys: {relation_p}, {doors}"%\n"
+    #         f"Items per m²: {relation}\n")
+    return relation_EF, relation_E, relation_HE, relation
 
 def count_floor(img):
     """
@@ -157,12 +156,62 @@ def count_enemies(img):
 
 def count_health_to_enemy(img):
     """
-        Normalise the number of hearts by the number of floor tiles.
-        """
+    Normalise the number of hearts by the number of floor tiles.
+    """
     health_mask= np.isin(img, healths)
     health_tile = np.sum(health_mask)
     relation_HE = health_tile/count_enemies(img)
     return relation_HE
+
+def call_metrics(generated_maps_folder, original_maps_folder):
+
+    n_metrics = 4
+    gen_files = os.listdir(generated_maps_folder)
+    orig_files = os.listdir(original_maps_folder)
+    paths = [generated_maps_folder, original_maps_folder]
+    gen_metrics = np.zeros((2, len(orig_files), n_metrics))
+
+    for d, dist in enumerate([gen_files, orig_files]):
+        for i, file in enumerate(dist):
+            file_path = os.path.join(paths[d], file)
+
+            img_txt = process_txt(file_path)
+            img = txt2image(img_txt)
+
+            corners_gen = count_corners(img_txt)  # compute number of corners
+            gen_metrics[d, i, 0] = corners_gen
+
+            relation_EF, relation_E, relation_HE, relation = item_distribution(img_txt)
+            gen_metrics[d, i, 1] = relation_EF
+            gen_metrics[d, i, 2] = relation_HE
+            gen_metrics[d, i, 3] = relation
+    
+    H_gen = categorical_entropy(generated_maps_folder)  # generated maps
+    H_orig = categorical_entropy(original_maps_folder)  # existing maps
+    entropy_metric = abs(H_gen - H_orig)
+    print(f"Delta Entropy metric = {entropy_metric}")
+
+    corners_gen = np.mean(gen_metrics[0,:,0])
+    corners_orig = np.mean(gen_metrics[1,:,0])
+    corners_metric = (corners_gen - corners_orig)
+    print(f"Difference in corners = {corners_metric}")
+
+    enemies_gen = np.mean(gen_metrics[0,:,1])
+    enemies_orig = np.mean(gen_metrics[1,:,1])
+    enemies_metric = (enemies_gen - enemies_orig)
+    print(f"Difference in enemies = {enemies_metric}")
+
+    health_gen = np.mean(gen_metrics[0,:,2])
+    health_orig = np.mean(gen_metrics[1,:,2])
+    health_metric = (health_gen - health_orig)
+    print(f"Difference in health = {health_metric}")
+
+    survive_gen = np.mean(gen_metrics[0,:,3])
+    survive_orig = np.mean(gen_metrics[1,:,3])
+    survive_metric = (survive_gen - survive_orig)
+    print(f"Difference in survivability = {survive_metric}")
+
+    return entropy_metric, corners_metric, enemies_metric, health_metric, survive_metric
 
 if __name__ == "__main__":
 
@@ -195,4 +244,6 @@ if __name__ == "__main__":
     print("Original Map")
     item_distribution(img_orig_txt)
     # SHould have items
-    # aantal doodlopende gangen
+
+    call_metrics("C:/Users/alhst/Documents/AI Master/Modern Game AI Algorithms/A3_DOOM/mgaia3/WFCGenerator/generated_maps",
+                 "C:/Users/alhst/Documents/AI Master/Modern Game AI Algorithms/A3_DOOM/mgaia3/WFCGenerator/test_map")
